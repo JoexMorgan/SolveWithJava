de06b04ce2d6cbe65af32ccd9c4940b3373d06ea
Joseph Morgan <joexmorgan@gmail.com>
2019-03-01 19:30:57 -0700

    Added method countCTG

--- Week 2 Project/StringsThirdAssignments/StringsThirdAssignments/Part2.java
+++ Week 2 Project/StringsThirdAssignments/StringsThirdAssignments/Part2.java
@@ -2,6 +2,7 @@
 /**
  * cgRatio counts the number of times 'C' or 'G' occurs
  * in a codon and returns the count divided by the codon's length
+ * countCTG counts the occurences of the codon CTG in a given dna string
  * 
  * @author joexcode
  * @version 96764.81
@@ -18,5 +19,22 @@ public class Part2 {
     }
     public void cgTest() {
         System.out.println(cgRatio("ATGCGCTAA"));
+    }
+    public int countCTG(String dna) {
+        int count = 0;
+        for (int i = 0; i <= dna.length() - 3; i++) {
+            if (dna.substring(i, i+3).equals("CTG")) {
+                count++;
+            }
+        }
+        return count;
+    }
+    public void countTest() {
+        System.out.println("ATGATTTAA " + countCTG("ATGATTTAA"));
+        //expected output: 0
+        System.out.println("ATGCTGTAG " + countCTG("ATGCTGTAG"));
+        //expected output: 1
+        System.out.println("CTGCTGCTGCTG " + countCTG("CTGCTGCTGCTG"));
+        //expected output: 4
     }
 }
