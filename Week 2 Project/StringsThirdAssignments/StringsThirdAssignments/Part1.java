b15bdab05a338e8afb6158621a0b6f1494a7f616
Joseph Morgan <joexmorgan@gmail.com>
2019-03-01 17:36:09 -0700

    Store all genes in a simplified array of type StorageResource

---
+++ Week 2 Project/StringsThirdAssignments/StringsThirdAssignments/Part1.java
@@ -0,0 +1,73 @@
+
+/**
+ * Write a description of Part1 here.
+ * 
+ * @author joexcode 
+ * @version 96764.62
+ */
+import edu.duke.*;
+public class Part1 {
+    public int findStopCodon(String dna, int startIndex, String stopCodon) {
+        int currIndex = dna.indexOf(stopCodon, startIndex+3);
+        while (currIndex != -1) {
+            int diff = currIndex - startIndex;
+            if (diff % 3 == 0) {
+                return currIndex;
+            } else {
+            currIndex = dna.indexOf(stopCodon, currIndex + 1);
+            }
+        }
+        return dna.length();
+    }
+    public String findGene(String dna, int where) {
+        int startIndex = dna.indexOf("ATG", where);
+        int taaIndex = findStopCodon(dna, startIndex, "TAA");
+        int tagIndex = findStopCodon(dna, startIndex, "TAG");
+        int tgaIndex = findStopCodon(dna, startIndex, "TGA");
+        int shortStop = Math.min(taaIndex, Math.min(tagIndex, tgaIndex));
+        if (startIndex == -1 || shortStop == dna.length()) {
+            return "";
+        }
+        return dna.substring(startIndex, shortStop + 3);
+    }
+    public void test() {
+        System.out.print("\n");
+        System.out.println(findStopCodon("ATGAATGGTATAA",4,"TAA"));
+    }
+    public void testFindGene() {
+        //System.out.println(findGene("ATTAAAGATAAAGGGAAAT"));
+        //System.out.println(findGene("AAATTTATGGAGTATGGTTAA"));
+        //System.out.println(findGene("AATGGTATAATGATAGAATTAA"));
+        //System.out.println(findGene("AATGGTATATTGGAATTATTGGGGGG"));
+    }
+    public void printAllGenes(String dna) {
+        int currIndex = 0;
+        while (true) {
+            String currGene = findGene(dna, currIndex);
+            if (currGene.isEmpty()) {
+                break;
+            }
+            System.out.println(currGene);
+            currIndex = dna.indexOf(currGene, currIndex) + currGene.length();
+        }
+        //System.out.println(printAllGenes("ATGTAATTTATGTGAAAAATGTAG"));
+    }
+    public StorageResource getAllGenes(String dna) {
+        int currIndex = 0;
+        StorageResource resource = new StorageResource();
+        while (true) {
+            String currGene = findGene(dna, currIndex);
+            if (currGene.isEmpty()) {
+                break;
+            }
+            //System.out.println(currGene);
+            resource.add(currGene);
+            currIndex = dna.indexOf(currGene, currIndex) + currGene.length();
+        }
+        //System.out.println(printAllGenes("ATGTAATTTATGTGAAAAATGTAG"));
+        return resource;
+    }
+    public void getTest() {
+        System.out.println(getAllGenes("ATGTAATTTATGTGAAAAATGTAG").data());
+    }
+}
