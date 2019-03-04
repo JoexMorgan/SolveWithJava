646910b794af1919a77b5de659d335688f195fd9
Joseph Morgan <joexmorgan@gmail.com>
2019-03-04 00:17:34 -0700

    2. countryInfo method added

--- Week 3/CSVProject/Exports.java
+++ Week 3/CSVProject/Exports.java
@@ -1,6 +1,6 @@
 
 /**
- * 
+ * Get info returns the exports and their dollar values of the country input
  * 
  * @author joexcode
  * @version 96770.73
@@ -12,15 +12,24 @@ public class Exports {
     public void tester() {
         FileResource fr = new FileResource("exports_small.csv");
         CSVParser parser = fr.getCSVParser();
-        for (CSVRecord r : parser) {
-         String s = r.get("Country");
-         System.out.println(s);
-        }
+        System.out.println(countryInfo(parser, "United States"));
+        //for (CSVRecord r : parser) {
+         //String s = r.get("Country");
+         //System.out.println(s);
+        //}
         //String s = parser.get("Name");
         //System.out.println(parser.get(0));   
     }
     public String countryInfo(CSVParser parser, String country) {
-        return null;
-        //return format '${country}: ${export}: ${value}'
+        String info = "";
+        for (CSVRecord r : parser) {
+           if (r.get("Country").equals(country)) {
+              String name = r.get("Country");
+              String export = r.get("Exports");
+              String values = r.get("Value (dollars)");
+              return name + ": " + export + ": " + values;
+           }
+        }
+        return "NOT FOUND";
     }
 }
